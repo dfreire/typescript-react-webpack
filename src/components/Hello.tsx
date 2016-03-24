3f835211b6dfca48e8efdd10dbfa5820fa392258
dfreire <dario.freire@gmail.com>
2016-03-24 23:56:34 +0000

    update

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface HelloProps {
+    compiler: string;
+    framework: string;
+}
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
